4577b77dfb325db8098a45724281e64f62d9e3b0
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-25 17:31:56 +0530

    With refs property implementng DOM manipulations without using selectors

--- model-project/src/App.vue
+++ model-project/src/App.vue
@@ -1,25 +1,46 @@
 <template>
-    <div>
-        <h1>{{title}}</h1>
-    </div>
+  <div>
+    <h1>{{ title }}</h1>
+    <input type="text" @input="handleInput" ref="textInput" />
+    <button @click="handleClick">Click Me</button>
+    <p ref="displaytext">{{text}}</p>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-    name: 'App',
-    data (){
-        return {
-            title: "My First Vue app :)"
-        }
+  name: "App",
+  data() {
+    return {
+      title: "My First Vue app :)",
+      text: "Type somthing and click the button"
+    };
+  },
+
+  methods: {
+    handleClick(){
+        console.log(this.$refs.textInput);
+        this.$refs.displaytext.classList.add("active");
+    },
+     handleInput(){
+        console.log(this.$refs.textInput.value);
+        this.text = this.$refs.textInput.value;
+
+        this.$refs.displaytext.classList.remove("active");
     }
-  
-}
+  },
+};
 </script>
 
 <style>
-    h1{
-        color: rgb(255, 83, 111);
-    }
+  h1 {
+    color: rgb(255, 83, 111);
+  }
+
+  .active{
+    color: skyblue;
+  }
 </style>
+
+
+aa
