437b6f71d11c914c5d23fba82d7bf2add4eadce0
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-25 11:15:35 +0530

    Defining method in Vue app and call them from HTML when click events are triggered from HTML

--- creating VUe app/app.js
+++ creating VUe app/app.js
@@ -7,6 +7,12 @@ const app = Vue.createApp({
             count : 0
         };
         
+    },
+
+    methods:{
+        resetCounter(){
+            this.count = 0;
+        }
     }
 });
 
@@ -16,3 +22,12 @@ app.mount('#app');
 
 
 
+/*
+
+    # We can defile methods in methods property in app component of Vue js
+
+    # whatever method we defined, later we can call them from HTML as well 
+
+    # Update data when we click from html element
+
+*/
