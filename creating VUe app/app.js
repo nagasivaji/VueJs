b58b3d774d5f3e12276d9e073a134ae5bbcef42c
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-25 11:05:54 +0530

    Click event from html to update data

--- creating VUe app/app.js
+++ creating VUe app/app.js
@@ -4,13 +4,9 @@ const app = Vue.createApp({
     // Vue app data
     data(){
         return {
-            title: 'Learn Vue by Net Ninja',
-            book : {
-                price : 250,
-                pages : 720,
-                eddition : 'Third Edition'
-            }
+            count : 0
         };
+        
     }
 });
 
@@ -20,16 +16,3 @@ app.mount('#app');
 
 
 
-
-/*
-
-    // Whatever the data presen in the data function in Vue app it will be accessible in htmlfil 
-
-    // The data type in data function can be anytype
-
-    // NOTE: The only limitation is that we can access the data into the html elements only on which the Vue app is mounted or linked to the tag.
-
-    // Outside of that html mounted tag we can not access the data
-
-
-*/ 
