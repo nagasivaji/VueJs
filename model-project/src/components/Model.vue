c24decd8dc9125741fe14c9896b6e7291ec6e4d5
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-25 18:03:07 +0530

    Scoped in styling component

---
+++ model-project/src/components/Model.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="backdrop">
+        <div class="model">
+            <p>model content</p>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+    .model{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+</style>
+
+/*
+    when we specify the style tag just normally like this <style ></style>
+    then the stylong inside this style tag will appply to globally 
+
+    To limit this styling to this specific component,
+    we have to spesify "scoped" in style tag like this <style scoped></style>
+
+
+    Then the stylings is applied to only this component.
+
+ */
