b78e4dd997b3a6bc2b9b2c5e348654b0d5c891f2
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-25 14:29:00 +0530

    Dynamic classing for HTML elements using VUE

--- creating VUe app/app.js
+++ creating VUe app/app.js
@@ -4,7 +4,15 @@ const app = Vue.createApp({
     // Vue app data
     data(){
         return {
-            url : "https://www.crct.in/"
+            numbers : [
+                {num: 1, isFav: false},
+                {num: 2, isFav: true},
+                {num: 3, isFav: true},
+                {num: 4, isFav: false},
+                {num: 5, isFav: true},
+                {num: 6, isFav: false},
+                
+            ]
         };
         
     },
@@ -22,13 +30,6 @@ app.mount('#app');
 
 /*
 
-    # for Binding attributes to a html tag from vue app it will be something different than simply assigning data variables
-
-
-    # <a v-bind:href="url">Go to CRCT</a>
-    we have to use "v-bind:attributename" to bind data dynamically to attributes in html page
-
-    #Sortcut fot v-bind
-    <a :href="url">Go to CRCT</a>
+    
 
 */
