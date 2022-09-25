1cd91c93b25748bf01f97aa3603c019bbd811302
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-25 17:57:28 +0530

    First component in Vue

--- model-project/src/App.vue
+++ model-project/src/App.vue
@@ -1,46 +1,23 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <input type="text" @input="handleInput" ref="textInput" />
-    <button @click="handleClick">Click Me</button>
-    <p ref="displaytext">{{text}}</p>
-  </div>
+    <div>
+        <h1>{{ title }}</h1>
+        <Model />
+    </div>
 </template>
 
 <script>
-export default {
-  name: "App",
-  data() {
-    return {
-      title: "My First Vue app :)",
-      text: "Type somthing and click the button"
-    };
-  },
+    // Importing Model component from component folder
+    import Model from "./components/Model.vue";
 
-  methods: {
-    handleClick(){
-        console.log(this.$refs.textInput);
-        this.$refs.displaytext.classList.add("active");
+    export default {
+        name: "App",
+        components : { Model },
+        data() {
+            return {
+                title: "My First Vue app :)",
+                text: "Type somthing and click the button"
+            };
     },
-     handleInput(){
-        console.log(this.$refs.textInput.value);
-        this.text = this.$refs.textInput.value;
-
-        this.$refs.displaytext.classList.remove("active");
-    }
-  },
 };
+
 </script>
-
-<style>
-  h1 {
-    color: rgb(255, 83, 111);
-  }
-
-  .active{
-    color: skyblue;
-  }
-</style>
-
-
-aa
