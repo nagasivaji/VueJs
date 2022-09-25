06f90939b5f2f24ff0c35f48d1bc6f3e2db8934b
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-25 10:47:09 +0530

    Using Vue variables in HTML  from Data function from app.js

--- creating VUe app/app.js
+++ creating VUe app/app.js
@@ -1,19 +1,14 @@
-// console.log("Hello Vue");
 
-
-// Importing Vue Create app for creating a Vue App
+// Importing
 const app = Vue.createApp({
-    // we can have data here to access the data in HTML 
-
-    // We can have function here to cal from HTML
-
-    // We can have template here as well to render those templates in HTML
-
-    template: '<h2>Hello Vue from Uve App</h2>'
+    // Vue app data
+    data(){
+        return {
+            title: 'Learn Vue by Net Ninja'
+        };
+    }
 });
 
 
-
-// Mounting this app to HTML page
-//  note : link we use export default ComponentName in react here we are mounting Vue app to html tag
+// Mounting
 app.mount('#app');
