cfec4feb9ac97c4e0b28e751006c32b806451661
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-25 12:00:55 +0530

    Attributes binding in html from Vue data

--- creating VUe app/app.js
+++ creating VUe app/app.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
     // Vue app data
     data(){
         return {
-            names:['Sushu', 'Shiva', 'Jerry', 'Tom']
+            url : "https://www.crct.in/"
         };
         
     },
@@ -22,6 +22,13 @@ app.mount('#app');
 
 /*
 
-    
+    # for Binding attributes to a html tag from vue app it will be something different than simply assigning data variables
+
+
+    # <a v-bind:href="url">Go to CRCT</a>
+    we have to use "v-bind:attributename" to bind data dynamically to attributes in html page
+
+    #Sortcut fot v-bind
+    <a :href="url">Go to CRCT</a>
 
 */
