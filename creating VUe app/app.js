72b14f325b07523171f8822ea79e74b462914368
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-25 10:33:49 +0530

    create Vue app and linking vue app to html

--- creating VUe app/app.js
+++ creating VUe app/app.js
@@ -1,1 +1,12 @@
+// console.log("Hello Vue");
 
+// Importing Vue Create app for creating a Vue App
+const app = Vue.createApp({
+    
+});
+
+
+
+// Mounting this app to HTML page
+//  note : link we use export default ComponentName in react here we are mounting Vue app to html tag
+app.mount('#app');
