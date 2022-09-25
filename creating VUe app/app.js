ac33c8c5af89b992adfbfa02d714d165044e2805
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-25 11:36:14 +0530

    If else implementation in html

--- creating VUe app/app.js
+++ creating VUe app/app.js
@@ -4,14 +4,20 @@ const app = Vue.createApp({
     // Vue app data
     data(){
         return {
-            count : 0
+            marks : this.refreshMarks(),
         };
         
     },
 
     methods:{
-        resetCounter(){
-            this.count = 0;
+        refreshMarks(){
+            console.log(this.marks);
+
+            const mark = Math.floor(Math.random(this.marks) * 100);
+
+            this.marks =  mark;
+            
+            return mark;
         }
     }
 });
@@ -24,10 +30,6 @@ app.mount('#app');
 
 /*
 
-    # We can defile methods in methods property in app component of Vue js
-
-    # whatever method we defined, later we can call them from HTML as well 
-
-    # Update data when we click from html element
+    
 
 */
