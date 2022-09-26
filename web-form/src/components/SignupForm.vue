1746fe9e6099cfacba26fe6a290a5c92bd2e2d87
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-26 16:33:45 +0530

    2way binding for select role

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -3,11 +3,19 @@
         <form>
             <label>Email: </label>
             <input type="email" required v-model="email">
+
             <label>Password: </label>
             <input type="password" required v-model="password">
+
+            <label>Role: </label>
+            <select v-model="role">
+                <option value="developer">Web Developer</option>
+                <option value="designer">Web Designer</option>
+            </select>
         </form>
         <p>Email: {{email}}</p>
         <p>Password: {{password}}</p>
+        <p>Role: {{role}}</p>
     </div>
 </template>
 
@@ -17,6 +25,7 @@ export default {
         return {
             email: "",
             password: "",
+            role: "developer",
         }
     },
 }
