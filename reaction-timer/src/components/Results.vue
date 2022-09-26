06f8e81ba2492a4e4768a54c0d98b481353af9c8
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-26 15:31:37 +0530

    Added some stylings and finished project

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,13 +1,34 @@
 <template>
-    <p>Reaction Time: {{this.time}}ms</p>
+    <div>
+        <p>Reaction Time: {{this.time}}ms</p>
+        <p class="rank">{{rank}}</p>
+    </div>
 </template>
 
 <script>
 export default {
     props : ["time"],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+
+    mounted() {
+        if(this.time < 250)
+            this.rank = "Ninja Fingers";
+        else if(this.time < 450)
+            this.rank = "Rapid Reflexes";
+        else
+            this.rank = "Snail pace";
+    },
 }
 </script>
 
 <style>
-
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
 </style>
