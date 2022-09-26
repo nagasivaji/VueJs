4c58791b12387f4b715afae4f259a2db149018cf
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-26 23:05:01 +0530

    Based on fetch results showing data otherwise LOADING...  text

--- ninja-jobs/src/views/jobs/jobDetails.vue
+++ ninja-jobs/src/views/jobs/jobDetails.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
+    <div v-if="status">
         <h1>Job Detils Page</h1>
-        <p>Job id is {{id}}</p>
+        <p>Job id is {{job.id}}</p>
+        <h2>Job title is {{job.title}}</h2>
+        <p>Job detils is {{job.details}}</p>
+    </div>
+    <div v-else>
+        <p>Loading....</p>
     </div>
 </template>
 
@@ -9,10 +14,28 @@
 export default {
     props: ['id'],
     data() {
-        // return {
-        //     id: this.$route.params.id,
-        // }
+        return {
+            job: {},
+            status: false,
+        }
     },
+    mounted() {
+        fetch("http://localhost:8000/jobs/"+this.id)
+        .then((res) =>{
+            //console.log(res.status);
+            if(res.status !== 404)
+                this.status = true
+            return res.json();
+        })
+        .then((data) =>{
+            this.job = data;
+            //console.log(data);
+        })
+        .catch((err) =>{
+            console.log(err.message);
+        });
+    },
+
 }
 </script>
 
--- ninja-jobs/src/views/jobs/jobs.vue
+++ ninja-jobs/src/views/jobs/jobs.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
         <h1>Jobs</h1>
-        <div v-for="job in jobs"  :key="job.id" class="job">
-            <router-link :to="{name: 'jobsDetails' , params:{ id: job.id }}">
-                <h2>{{ job.title }}</h2>
-            </router-link>
+        <div v-if="jobs.length">
+            <div v-for="job in jobs"  :key="job.id" class="job">
+                <router-link :to="{name: 'jobsDetails' , params:{ id: job.id }}">
+                    <h2>{{ job.title }}</h2>
+                </router-link>
+            </div>
+        </div>
+        <div v-else>
+            <p>Loading Jobs....</p>
         </div>
     </div>
 </template>
