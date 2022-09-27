8acb0b50df02360ccaa3e55f80fe3ae54e1690ca
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-27 15:11:38 +0530

    Life cycle hooks in setup()

---
+++ dojo-blog/src/components/PostList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <SinglePost :post="post" />
+        </div>
+    </div>
+</template>
+
+<script>
+// impoering components
+import SinglePost from './SinglePost.vue';
+import { onMounted } from 'vue';
+
+export default {
+    props: ['posts'],
+    components: { SinglePost},
+    setup(props) {
+        console.log(props);
+        // Life cycle hooks in setup()
+        onMounted(console.log("Mounted"));
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ dojo-blog/src/components/SinglePost.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="post">
+        <h3>{{post.title}}</h3>
+        <p>{{snippet}}</p>
+    </div>
+</template>
+
+<script>
+// importing computed hook
+import { computed } from 'vue';
+export default {
+    props: ["post"],
+    setup(props) {
+        const snippet = computed(() => {
+            return props.post.body.length > 100 ? props.post.body.substring(0, 99) + "....": props.post.body;
+        });
+
+
+        return {snippet};
+    }
+}
+</script>
+
+<style>
+
+</style>
