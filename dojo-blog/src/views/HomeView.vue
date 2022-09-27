25c906a938a2a2f29287b055acd071696ce6dda4
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-27 11:53:33 +0530

    defining methods in setup hook

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -4,6 +4,7 @@
         <!-- Data comming from setup function -->
         <p>{{name}}</p>
         <p>{{EmpId}}</p>
+        <button @click="handleClick">Click ME</button>
     </div>
 </template>
 
@@ -17,7 +18,9 @@ export default {
         let name = "sivaji";
         let id = 22;
 
-        return {name, EmpId: id};
+        const handleClick = () => console.log("CLICKED");;
+
+        return {name, EmpId: id, handleClick};
     },
     
 }
@@ -31,6 +34,16 @@ export default {
     # From above return statement we can obser two diffrent patterns
     # Here name is retured as name. So we can use that as name in our components
     # But id is returned as EmpId. So we can use id as EmpId in our components.
+
+
+    # If we want to access function defined in setup then they also must be returned from setup     function as show in above example
+
+    ``````````````````````````````````` NOTE ````````````````````````````````````````````````````
+
+    # If tha data in the data hook gets chnaged it will reflect the data in the component as well. Because they are REACTIVE data
+
+    # If tha data in the setup hook gets chnaged it will not reflect the data in the component. Because they are NOT REACTIVE data
+
 
 
  */
