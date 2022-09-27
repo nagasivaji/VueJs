403e4b87132cd2cf920df53ed2abb9a1759bbb21
NagaSivaji <nagasivajisirigineedi@gmail.com>
2022-09-27 15:08:17 +0530

    working with props in setup()

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,41 +1,37 @@
 <template>
     <div class="home">
         <h1>HOME</h1>
-        <input type="text" v-model="search">
-        <p>Serch: {{search}}</p>
-        <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+        <PostList :posts= 'posts' />
+
     </div>
 </template>
 
 <script>
+// importing components
+import PostList from '../components/PostList.vue';
 // Importing refs
-import { computed, ref, watch, watchEffect } from "vue";
+import { ref } from "vue";
+
 
 export default {
     name: "HomeView",
+    components: { PostList },
 
     setup() {
-        const search = ref("")
-        const names = ref(['mario', 'yoshi', 'luigi', 'toda', 'bowser', 'koopa', 'peach']);
 
-        // watch function will trigget whenever any chnage in the value of a variable which is passed to whatch function, in our case it is search
-        watch(search, () =>{
-            console.log('Watch function fired');
-        });
+        const posts = ref([
+            {
+                title: 'Welcometo the blog',
+                body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,',
+                id: 1
+            }, 
+            {
+                title: 'Top 5 CSS tips',
+                body: 'Lorem ipsum',
+                id: 2
+            }]);
 
-
-        // Watch Effect function will trigger whenever the setup() function is fired for the first time.
-        watchEffect(() => {
-            console.log('Watch effect function fired');
-        });
-
-        const matchingNames = computed(() =>{
-            return names.value.filter((name) =>{
-                return name.includes(search.value);
-            });
-        });
-
-        return { names , search, matchingNames};
+        return { posts };
     },
 };
 </script>
