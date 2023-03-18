605a3803b50f1bfcc23a98c9b678b13e019b023e
NagaSivaji <nagasivajisirigineedi@gmail.com>
2023-03-18 11:15:29 +0530

    Passinf more data with constant url

--- VUE js Part 2 - UDEMY/test-app-3/src/App.vue
+++ VUE js Part 2 - UDEMY/test-app-3/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div id="nav">
-      <router-link :to="{name: 'home', query: {abc: 'This is home'}}">Home</router-link>
-      <router-link :to="{name: 'about', query: {abc: 'This is About'}}">About</router-link>
+      <router-link :to="{name: 'home', query: {abc: 'This is home', xyz: 259}}">Home</router-link>
+      <router-link :to="{name: 'about', query: {abc: 'This is About', xyz: 289}}">About</router-link>
       <!-- <router-link :to="'/home/abc'">Home</router-link>
       <router-link :to="'/about/abc'">About</router-link> -->
     </div>
